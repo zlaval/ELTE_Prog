7c058191097aaa97ede9d8d89c7b169da0e8c482
zlaval <zlaval@gmail.com>
2020-10-11 11:44:50 +0200

    wip graph1

--- algo2/kotlin/src/main/kotlin/com/zlrx/algo2/graph1/G2AjdMrx.kt
+++ algo2/kotlin/src/main/kotlin/com/zlrx/algo2/graph1/G2AjdMrx.kt
@@ -1,2 +1,41 @@
 package com.zlrx.algo2.graph1
 
+fun main() {
+    val matrix = arrayOf(
+        arrayOf(0, 1, 0, 1, 0),
+        arrayOf(0, 0, 0, 1, 1),
+        arrayOf(1, 0, 0, 0, 0),
+        arrayOf(0, 0, 0, 0, 0),
+        arrayOf(0, 1, 1, 0, 0)
+    )
+
+    val result = toG2(matrix)
+    printResult(result)
+
+}
+
+private fun toG2(matrix: Array<Array<Int>>): Array<Array<Int>> {
+    val result = Array(matrix.size) { Array(matrix.size) { 0 } }
+    matrix.withIndex().forEach { (i, row) ->
+        row.withIndex().forEach { (j, value) ->
+            if (value == 1) {
+                matrix[j].withIndex().forEach { (k, v) ->
+                    if (v == 1 && i!=k) {
+                        result[i][k] = 1
+                    }
+                }
+            }
+        }
+    }
+    return result
+}
+
+
+private fun printResult(adjMtx: Array<Array<Int>>) {
+    adjMtx.forEach { row ->
+        row.forEach {
+            print("$it ")
+        }
+        print(System.lineSeparator())
+    }
+}
