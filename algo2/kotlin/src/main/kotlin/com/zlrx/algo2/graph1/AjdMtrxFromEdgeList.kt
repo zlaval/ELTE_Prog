db777f2b1219033282cc619950d0f1f094eb5f96
zlaval <zlaval@gmail.com>
2020-10-11 11:00:00 +0200

    wig graph1

--- algo2/kotlin/src/main/kotlin/com/zlrx/algo2/graph1/AjdMtrxFromEdgeList.kt
+++ algo2/kotlin/src/main/kotlin/com/zlrx/algo2/graph1/AjdMtrxFromEdgeList.kt
@@ -1,2 +1,34 @@
 package com.zlrx.algo2.graph1
 
+fun main() {
+    val edgeList = arrayListOf(
+        Edge(1, Edge(3)),
+        Edge(3, Edge(4)),
+        Edge(0),
+        null,
+        Edge(1, Edge(2))
+    )
+    val result = transform(edgeList)
+    printResult(result)
+}
+
+fun transform(edgeList: List<Edge?>): Array<Array<Int>> {
+    val array = Array(edgeList.size) { Array(edgeList.size) { 0 } }
+    edgeList.withIndex().forEach { (i, e) ->
+        var edge = e
+        while (edge != null) {
+            array[i][edge.value] = 1
+            edge = edge.next
+        }
+    }
+    return array
+}
+
+private fun printResult(adjMtx: Array<Array<Int>>) {
+    adjMtx.forEach { row ->
+        row.forEach {
+            print("$it ")
+        }
+        print(System.lineSeparator())
+    }
+}
--- algo2/kotlin/src/main/kotlin/com/zlrx/algo2/graph1/FindUNeighboursInEdgeList.kt
+++ algo2/kotlin/src/main/kotlin/com/zlrx/algo2/graph1/FindUNeighboursInEdgeList.kt
@@ -1,2 +1,25 @@
 package com.zlrx.algo2.graph1
 
+fun main() {
+    val edgeList = arrayListOf(
+        Edge(1, Edge(3)),
+        Edge(0, Edge(3)),
+        Edge(3),
+        Edge(0, Edge(1, Edge(2)))
+    )
+
+    findNeighboursOf(2, edgeList)
+}
+
+private fun findNeighboursOf(u: Int, edgeList: List<Edge?>) {
+    edgeList.withIndex().forEach { (i, e) ->
+        var edge = e
+        while (edge != null && edge.value < u) {
+            edge = edge.next
+        }
+        if (edge != null && edge.value == u) {
+            println(i+1)
+        }
+    }
+
+}
