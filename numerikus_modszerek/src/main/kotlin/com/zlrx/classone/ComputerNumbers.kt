7f172d2961506170e500ab9e70872a5a1a0c0aa9
zlaval <zlaval@gmail.com>
2022-02-13 12:25:07 +0100

    calculate computer number

--- numerikus_modszerek/src/main/kotlin/com/zlrx/classone/ComputerNumbers.kt
+++ numerikus_modszerek/src/main/kotlin/com/zlrx/classone/ComputerNumbers.kt
@@ -1,5 +1,6 @@
 package com.zlrx.classone
 
+import java.lang.Integer.min
 import kotlin.math.abs
 import kotlin.math.pow
 
@@ -14,8 +15,11 @@ import kotlin.math.pow
 
 fun main() {
     val computerNumbers = ComputerNumbers()
-    computerNumbers.calculateSmallest(6, -10, 10)
-    computerNumbers.calculateLargest(6, -10, 10)
+    computerNumbers.calculateSmallest(6, -3, 3)
+    computerNumbers.calculateLargest(6, -3, 3)
+    computerNumbers.calculateFlInt(137, 6, -10, 10)
+    computerNumbers.calculateFlFraction(17, 8, -5, 5)
+    computerNumbers.calculateFlFraction(167, 8, -5, 5)
 }
 
 
@@ -31,6 +35,7 @@ class ComputerNumbers {
         }
     }
 
+    //(1-2^-k+)*2^k+
     fun calculateLargest(t: Int, km: Int, kp: Int) {
         val abspow = 2.0.pow(abs(kp))
 
@@ -46,6 +51,63 @@ class ComputerNumbers {
 
         val realResult = (dividend / maxDivider) * (2.0.pow(kp))
         println("Real result: $realResult")
+
+    }
+
+    fun calculateFlInt(number: Int, t: Int, km: Int, kp: Int) {
+        //Integer.toBinaryString
+        var num = number
+        var result = ""
+        while (num > 0) {
+            val mod = num % 2
+            num = (num - mod) / 2
+            result += mod
+        }
+        result = result.reversed()
+        val characteristic = min(result.length, kp)
+        val roundDigit = result.substring(t until t + 1)
+
+        val digits = if (roundDigit == "1") {
+            val value = result.substring(0 until t)
+            val intVal = Integer.parseInt(value, 2)
+            val strVal = Integer.toBinaryString(intVal + 1)
+            strVal.substring(0 until t)
+        } else {
+            result.substring(0 until t)
+        }
+
+        val resultBinStr = digits + (0 until (characteristic - digits.length)).joinToString("") { "0" }
+        val resultInt = Integer.parseInt(resultBinStr, 2)
+        println("[$digits | $characteristic] = $resultInt")
+    }
+
+    fun calculateFlFraction(fraction: Int, t: Int, km: Int, kp: Int) {
+        val barrier = ("1" + (0 until fraction.toString().length).joinToString("") { "0" }).toInt()
+
+        var significantBitCount = 0
+        var bitCount = 0
+        var number = fraction
+        var result = ""
+        while (significantBitCount < t && number != 0) {
+            number *= 2
+            if (number > barrier) {
+                result += "1"
+                significantBitCount++
+                number -= barrier
+            } else {
+                result += "0"
+                if(significantBitCount>0){
+                    significantBitCount++
+                }
+            }
+            bitCount++
+
+        }
+
+        val c = -1 * (bitCount - significantBitCount)
+        val significantBits = result.substring(bitCount - significantBitCount)
+
+        println("fl(0.$fraction) = [$significantBits | $c]")
 
     }
 
